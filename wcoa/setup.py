8e8184b46fd158b436225eab4606cac40539dabf
David Pollard <pollardld@gmail.com>
2019-07-12 16:58:20 -0700

    Add setup file and requirement

--- wcoa/setup.py
+++ wcoa/setup.py
@@ -8,12 +8,12 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-polls',
+    name='wcoa',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    license='BSD License',  # example license
-    description='A simple Django app to conduct Web-based polls.',
+    license='MIT License',  # example license
+    description='WCOA',
     long_description=README,
     url='https://www.example.com/',
     author='Your Name',
@@ -21,9 +21,9 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
+        'Framework :: Django :: 2.2',  # replace "X.Y" as appropriate
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',  # example license
+        'License :: OSI Approved :: MIT License',  # example license
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
