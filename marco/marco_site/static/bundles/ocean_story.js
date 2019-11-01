5ce8bb33fbad89a3c733da1f534d54bcaec63886
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-11-01 16:03:24 -0700

    fixing Ocean Story logic to use layer order and opacity from URL hash

--- marco/marco_site/static/bundles/ocean_story.js
+++ marco/marco_site/static/bundles/ocean_story.js
@@ -557,8 +557,36 @@
 	    return dataLayers[id];
 	  }
 
-	  function setDataLayers(layers) {
+	  function setDataLayers(layers, hashLayerOverrides) {
 	    var layerKeys = Object.keys(layers)
+	    var overrideKeys = Object.keys(hashLayerOverrides);
+
+	    layerKeys.sort(function(a, b) {
+	        if (hashLayerOverrides.hasOwnProperty(a) && hashLayerOverrides[a].hasOwnProperty('order')) {
+	           if (hashLayerOverrides.hasOwnProperty(b) && hashLayerOverrides[b].hasOwnProperty('order')) {
+	             return hashLayerOverrides[a].order - hashLayerOverrides[b].order;
+	          }
+	        }
+	        return true;
+	    });
+
+	    for (var i = 0; i < layerKeys.length; i++) {
+	      if (overrideKeys.indexOf(layerKeys[i]) >= 0) {
+	        var layer = layers[layerKeys[i]];
+	        var override = hashLayerOverrides[layerKeys[i]];
+	        var l = fetchDataLayer(layer.id);
+	        l.setOpacity(override.opacity);
+	        if (!l.hasOwnProperty('state_') || !l.state_) {
+	          l.state_ = {};
+	        }
+	        l.state_.zIndex = override.order;
+	        l.state_.opacity = override.opacity;
+	        l.state_.visible = override.display;
+	        if (!l.state_.hasOwnProperty('layer')) {
+	          l.state_.layer = l;
+	        }
+	      }
+	    }
 
 	    // trim unused layers
 	    _.each(_.difference(visibleDataLayers, layerKeys), function(id) {
@@ -578,6 +606,32 @@
 	    visibleDataLayers = layerKeys;
 	  }
 
+	  function parseHash(url) {
+	    var hashObject = {};
+	    var hashParams = new URLSearchParams(url);
+	    var layerParams = hashParams.getAll('dls[]');
+	    var layerOrder = 0;
+	    while(layerParams.length > 2) {
+	      // RDH: this handles a case where both layer id and opacity are 1
+	      var newHashLayer = {'id':1, 'opacity':1, 'order': layerOrder};
+	      for (var i = 0; i < 3; i++) {
+	        if ([true, "True", 'true'].indexOf(layerParams[i])>= 0 ) {
+	          newHashLayer.display = true;
+	        } else if ( layerParams[i] >= newHashLayer.id && layerParams[i].toString().indexOf('.') < 0) {
+	          newHashLayer.id = parseInt(layerParams[i]);
+	        } else if (parseFloat(layerParams[i]) <= 1) {
+	          newHashLayer.opacity = parseFloat(layerParams[i]);
+	        }
+	      }
+	      if (newHashLayer.hasOwnProperty('display') && newHashLayer.display) {
+	        hashObject[newHashLayer.id.toString()] = newHashLayer;
+	      }
+	      layerParams = layerParams.slice(3);
+	      layerOrder ++;
+	    }
+	    return hashObject;
+	  }
+
 	  return {
 	    goToSection: function(section) {
 	      if (section > story.sections.length - 1) {
@@ -588,7 +642,7 @@
 
 	      engine.setView(s.view.center, s.view.zoom, function(){
 	        setBaseLayer(s.baseLayer);
-	        setDataLayers(s.dataLayers);
+	        setDataLayers(s.dataLayers, parseHash(s.url));
 	      });
 	    },
 	  };
