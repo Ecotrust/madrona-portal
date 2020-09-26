6618c5f35fd7e631000ddb417225e94ffa76eb72
Ryan Hodges <ryan.d.hodges@gmail.com>
2020-09-25 17:03:57 -0700

    fixing broken pw change bug - ex cont to be added in admin file

--- marco/marco/urls.py
+++ marco/marco/urls.py
@@ -45,7 +45,7 @@ urlpatterns += [
     #'',
     re_path(r'^sitemap\.xml$', sitemap),
 
-    path('django-admin/', admin.site.urls, {'extra_context':{'CATALOG_TECHNOLOGY': settings.CATALOG_TECHNOLOGY}}),
+    path('django-admin/', admin.site.urls),
 
     re_path(r'^rpc$', serve_rpc_request),
 
