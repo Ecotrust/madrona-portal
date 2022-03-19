9b72548511d8050bcdea4706c77de8b147bb8f80
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-03-18 19:19:04 -0700

    no longer need to invert coordinates during export

--- marco/marco/settings.py
+++ marco/marco/settings.py
@@ -444,7 +444,7 @@ GEOMETRY_CLIENT_SRID = 3857 #for latlon
 GEOJSON_SRID = 3857
 
 GEOJSON_DOWNLOAD = True  # force headers to treat like an attachment
-SUPPORT_INVERTED_COORDINATES = True
+SUPPORT_INVERTED_COORDINATES = False
 
 # authentication
 SOCIAL_AUTH_NEW_USER_URL = '/account/?new=true&login=django'
